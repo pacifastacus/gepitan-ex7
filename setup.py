2b73ad80017210f98d69e60c0f3a6938f61573e5
Palkovics Dénes <palkovics.denes@gmail.com>
2020-11-26 00:58:08 +0100

    Update setup.py
    (cherry picked from commit ec0515e5c82e7b897cf095bafd09b1d5bdebbcea)

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-def main():
-
-
-if __name__ == "main":
-    main()
+setup(
+   name='ML - K-Means Clustering and Principal Component Analysis - EX7',
+   version='1.0',
+   description='',
+   author='Dénes Palkovics',
+   author_email='palkovics.denes@gmail.com',
+   packages=['ex7'],
+   install_requires=[
+       'numpy',
+       'matplotlib',
+       'pickle'
+   ],
+   scripts=[]
+)
